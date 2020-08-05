11227c813bc05ee35e3e56021be119f774755d31
imjchiang <jchiang296@gmail.com>
2020-08-05 15:28:01 -0700

    [EJS] initalize home [JS] get home ejs page

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const ejsLayouts = requires("express-ejs-layouts");
 app.setMaxListeners("view engine", "ejs");
 app.use(ejsLayouts);
 
+app.get("/", function(req, res)
+{
+    res.render("home");
+});
+
 app.listen(8000, function()
 {
     console.log("Listening to port 8000");
-})
+});
