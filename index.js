1f2fa2654ebd6f18d7e3a4093480f53167b758dd
imjchiang <jchiang296@gmail.com>
2020-08-05 15:43:47 -0700

    [EJS] implement animals and foods pages

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get("/", function(req, res)
     res.render("home");
 });
 
+app.get("/animals", function(req, res)
+{
+    res.render("animals", {title: "Favorite Animals", animals:["sand crab", "corny joke dog"]});
+});
+
+app.get("/foods", function(req,res)
+{
+    res.render("foods", {title: "Favorite Foods", foods:["potatoes", "baked potatoes", "mashed potatoes", "sweet potatoes"]});
+});
+
 app.listen(3000, function()
 {
     console.log("Listening to port 3000");
